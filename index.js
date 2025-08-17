04488ad079eda95bda595b83fa99262c1adf7a31
bjoern.sol <bjoern.wagner@gmail.com>
2025-08-17 23:28:36 +0200

    feat: updates for evm scaffold support

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-require('@dialectlabs/create-blink-scaffold');
+
+const { execSync } = require('child_process');
+
+try {
+    // Pass all arguments to the original package
+    const args = process.argv.slice(2).join(' ');
+
+    // Run the core package directly
+    console.log('Forwarding to @dialectlabs/create-blink-scaffold...');
+    execSync(`npx @dialectlabs/create-blink-scaffold@2.0.0 ${args}`, {
+        stdio: 'inherit'
+    });
+} catch (error) {
+    // The error will already be displayed due to stdio: 'inherit'
+    process.exit(error.status || 1);
+}
